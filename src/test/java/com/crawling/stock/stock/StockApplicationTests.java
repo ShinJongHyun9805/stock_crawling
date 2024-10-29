e4933a1c725b2d77521ddcd439ea330b1572320b
jhshin <jakie580@gmail.com>
2024-10-29 23:02:30 +0900

    초기 설정

---
+++ src/test/java/com/crawling/stock/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crawling.stock.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
