d26a89b3357d156cbb1d591610c085d64aa5d36f
jhshin <jakie580@gmail.com>
2024-11-01 23:33:07 +0900

    yaml 설정 + member 추가

--- src/main/java/com/crawling/stock/stock/StockApplication.java
+++ src/main/java/com/crawling/stock/stock/StockApplication.java
@@ -2,12 +2,26 @@ package com.crawling.stock.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class StockApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString()); // -> spring security로 변환
 	}
 
 }
---
+++ src/main/java/com/crawling/stock/stock/domain/entity/MemberEntity.java
@@ -0,0 +1,28 @@
+package com.crawling.stock.stock.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    Long memberSeq;             // 회원 일련 번호
+
+    String memberId;            // 회원 ID
+
+    String name;                // 회원 명
+
+    String role;                // 권한
+
+}
